920050e9e5b7986d543ce0d2cbe0a5dcd23d19f4
erans <eran.shtekel@gmail.com>
2023-01-19 10:00:43 +0200

    final version

--- SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -6,18 +6,32 @@ public class StompServer {
 
     public static void main(String[] args) {
 
+        if (args.length != 2) {
+            System.out.println("missing argument: TPC or reactor");
+            return;
+        }
+
+        if (args[1].equals("tpc")){
         Server.threadPerClient(
                 7777, //port
                 () -> new StompProtocol<String>(), //protocol factory
                 StompEncoderDecoder::new //message encoder decoder factory
         ).serve();
+        }
+        else if (args[1].equals("reactor")){
+            Server.reactor(
+                Runtime.getRuntime().availableProcessors(),
+                7777, //port
+                () ->  new StompProtocol<String>(), //protocol factory
+                StompEncoderDecoder::new //message encoder decoder factory
+        ).serve();
+        }
+        else{
+            System.out.println("invalid argument: TPC or reactor");
+            return;
+        }
 
-        // Server.reactor(
-        //         Runtime.getRuntime().availableProcessors(),
-        //         7777, //port
-        //         () ->  new StompProtocol<String>(), //protocol factory
-        //         StompEncoderDecoder::new //message encoder decoder factory
-        // ).serve();
+        
 
 
     }
